d01e7a293ee504279f84166b7d7bc7fa7d52cb67
Hugo Sobral <hugosobral10@gmail.com>
2021-09-06 13:53:41 -0300

    Criaçao do algoritmo para agendamento das materias

--- src/utils/IntervalScheduling.js
+++ src/utils/IntervalScheduling.js
@@ -10,14 +10,76 @@ function montaGrade() {
     return grade;
 }
 
-var traduzDia = (dia) => {
-    if      (dia == "SEG") return 0;
-    else if (dia == "TER") return 1;
-    else if (dia == "QUA") return 2;
-    else if (dia == "QUI") return 3;
-    else if (dia == "SEX") return 4;
-    else if (dia == "SAB") return 5;
-    else return -1;
+var traduzDia = (dia) => dia-2;
+
+var traduzHorario = (periodo, horario) => {
+    var offset
+    if (periodo == 'M') offset = -1;
+    if (periodo == 'T') offset = 4;
+    if (periodo == 'N') offset = 10;
+    
+    return horario+offset;
+};
+
+function traduzHorarioSigaa(horario) {
+    const padrao = /\b([2-7]{1,6})([MTN])([1-7]{1,7})\b/;
+    const res = horario.match(padrao);
+    
+    if (res == null) return null;
+    else return {
+        'match': res[0],
+        'dias': res[1],
+        'periodo': res[2],
+        'horario': res[3]
+    }
 }
 
-var traduzHorario = (horario) => horario-8;
+function verificaCompatibilidade(grade, horarioMapeado) {
+    var _dias = horarioMapeado.dias;
+    var dias = [];
+    for (var i=0; i<_dias.length; i++) {
+        dias.push(traduzDia(parseInt(_dias[i])));
+    }    
+    var periodo = horarioMapeado.periodo;
+    var horario = horarioMapeado.horario;
+
+    var retorno = [];
+
+    for (const dia of dias) {
+        for (var i=0; i<periodo.length; i++) {
+            for (var j=0; j<horario.length; j++) {
+                var index = traduzHorario(periodo[i], parseInt(horario[j]));
+                if (grade[dia][index] != "-") return [false, null];
+                else retorno.push([dia, index]);
+            }
+        }
+    }
+
+    return [true, retorno];
+}
+
+function atualizaGrade(grade, posicoes, nome) {
+    for (const posicao of posicoes) {
+        var i = posicao[0];
+        var j = posicao[1];
+
+        grade[i][j] = nome;
+    }
+}
+
+export default function agendaGrade(listaDeMaterias) {
+    var grade = montaGrade();
+    var numPrioridades = listaDeMaterias.length;
+    for (var i=0; i<numPrioridades; i++) {
+        for (const materia of listaDeMaterias[i]) {
+            const verifica = verificaCompatibilidade(grade,  traduzHorarioSigaa(materia.horario)); 
+            if (verifica[0]) {
+                atualizaGrade(grade, verifica[1], materia.nome);
+            } 
+            else console.log('tá ocupado');
+        }
+    }
+
+    return grade;
+}
+
