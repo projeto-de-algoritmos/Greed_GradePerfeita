f8eadc51a4318ae0b9b265b896959c1bbfbe5f09
Hugo Sobral <hugosobral10@gmail.com>
2021-09-06 14:18:49 -0300

    Adiçao de comentarios ao codigo

--- src/utils/IntervalScheduling.js
+++ src/utils/IntervalScheduling.js
@@ -1,4 +1,7 @@
-function montaGrade() {
+/**
+   * Monta uma matriz de tamanho 6x14 representando os horários disponíveis durante a semana (SEGUNDA A SABADO DE 08 AS 22H)
+   */
+function montaTabela() {
     grade = [];
     for (var i=0; i<6; i++) {
         grade.push([]);
@@ -10,8 +13,19 @@ function montaGrade() {
     return grade;
 }
 
+
+/**
+   * Função que recebe o dia e retorna o index correspondente na tabela de horários.
+   * @param  {Number} dia  Número do dia traduzido do padrão de horários do SIGAA
+   */
 var traduzDia = (dia) => dia-2;
 
+
+/**
+   * Função que traduz o período da aula e devolve o index correspondente na tabela de horários
+   * @param  {String} periodo  Dia da aula traduzido do padrão de horários do SIGAA
+   * @param  {Number} horario  Horário da aula traduzido do padrão de horários do SIGAA
+   */
 var traduzHorario = (periodo, horario) => {
     var offset
     if (periodo == 'M') offset = -1;
@@ -21,6 +35,11 @@ var traduzHorario = (periodo, horario) => {
     return horario+offset;
 };
 
+
+/**
+   * Função que recebe o horário da aula no formato do SIGAA e devolve um objeto traduzindo os dias, período e horários.
+   * @param  {String} horario  Horário da aula no formato do SIGAA.
+   */
 function traduzHorarioSigaa(horario) {
     const padrao = /\b([2-7]{1,6})([MTN])([1-7]{1,7})\b/;
     const res = horario.match(padrao);
@@ -34,6 +53,12 @@ function traduzHorarioSigaa(horario) {
     }
 }
 
+
+/**
+   * Função que verifica se o horário da matéria já está ocupado com outra disciplina.
+   * @param  {Array<Array<String>>} grade  Tabela de horários.
+   * @param  {Object} horarioMapeado       Horário já traduzido do padrão sigaa.
+   */
 function verificaCompatibilidade(grade, horarioMapeado) {
     var _dias = horarioMapeado.dias;
     var dias = [];
@@ -58,6 +83,13 @@ function verificaCompatibilidade(grade, horarioMapeado) {
     return [true, retorno];
 }
 
+
+/**
+   * Função que atualiza os horários da grade horária com o nome da matéria
+   * @param  {Array<Array<String>>} grade     Tabela de horários.
+   * @param  {Array<Array<Number>>} posicoes  Conjunto de index da matéria.
+   * @param  {String} nome                    Nome da matéria.
+   */
 function atualizaGrade(grade, posicoes, nome) {
     for (const posicao of posicoes) {
         var i = posicao[0];
@@ -67,8 +99,12 @@ function atualizaGrade(grade, posicoes, nome) {
     }
 }
 
-export default function agendaGrade(listaDeMaterias) {
-    var grade = montaGrade();
+/**
+   * Função que monta a grade de horários
+   * @param  {Array<Array<Object>>} listaDeMaterias  Lista de lista de matérias ordenada a partir da prioridade das matérias
+   */
+export default function montaGrade(listaDeMaterias) {
+    var grade = montaTabela();
     var numPrioridades = listaDeMaterias.length;
     for (var i=0; i<numPrioridades; i++) {
         for (const materia of listaDeMaterias[i]) {
